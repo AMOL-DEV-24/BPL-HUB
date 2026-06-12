8d7923e4fff7117097a6fcd56758de22561cf8a6
Amol Pawar <amolpawar.dev@gmail.com>
2026-06-12 18:23:39 +0530

    Add global styles and layout component for the application

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import ReduxProvider from "@/providers/ReduxProvider";
+
+import "@/styles/main.scss";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ReduxProvider>{children}</ReduxProvider>
+      </body>
+    </html>
+  );
+}
